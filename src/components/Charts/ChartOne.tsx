cca7f5982593cfdbec6366b5b39148b71b89a4e8
ndaleghnoelalummbah <ndaleghnoelalum@gmail.com>
2024-07-17 08:45:31 +0100

    fix: type fixes

--- src/components/Charts/ChartOne.tsx
+++ src/components/Charts/ChartOne.tsx
@@ -3,8 +3,6 @@ import React, { useState } from "react";
 import ReactApexChart from "react-apexcharts";
 import ChartVariable from "../ChartVariable/ChartVariable";
 
-
-
 interface ChartOneState {
   series: {
     name: string;
@@ -16,11 +14,14 @@ type ChartOneProps = {
   options: ApexOptions;
   series: { name: string; data: number[] }[];
   chartLabel: { color: string; name: string }[];
-  heading: string
-}
-
-
-const ChartOne: React.FC<ChartOneProps> = ({ options, series, chartLabel, heading }) => {
+  heading: string;
+};
+const ChartOne: React.FC<ChartOneProps> = ({
+  options,
+  series,
+  chartLabel,
+  heading,
+}) => {
 
   const [state, setState] = useState<ChartOneState>({
     series: series,
@@ -34,7 +35,12 @@ const ChartOne: React.FC<ChartOneProps> = ({ options, series, chartLabel, headin
   handleReset;
 
   return (
-    <div className="col-span-12 mt-8 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
+    <div className="col-span-12 mt-8 rounded-sm border border-stroke bg-white px-5 pb-5 pt-12 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
+      <div>
+        <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+          {heading}
+        </h4>
+      </div>
       <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
         <div className="flex w-full flex-wrap gap-3 sm:gap-5">
           <ChartVariable chartLabel={chartLabel} />
@@ -57,3 +63,4 @@ const ChartOne: React.FC<ChartOneProps> = ({ options, series, chartLabel, headin
 };
 
 export default ChartOne;
+
